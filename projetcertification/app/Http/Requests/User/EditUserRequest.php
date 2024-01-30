965305a84c17b50ef6f1923fd4b39fa438ba0ceb
Jeanne Diop <diopjeannedarc@gmail.com>
2024-01-30 11:06:54 +0000

    quelque mise à jour

--- projetcertification/app/Http/Requests/User/EditUserRequest.php
+++ projetcertification/app/Http/Requests/User/EditUserRequest.php
@@ -29,7 +29,7 @@ class EditUserRequest extends FormRequest
             'nom' => 'required|string|max:255',
             'prenom' => 'required|string|max:255',
             'email' => 'required|string|email|max:255',
-            'password' => 'required|string|max:255',
+            'password' => 'required|min:8',
             'telephone' => ['required', 'regex:/^\+221(77|78|76|70)\d{7}$/'],
             // 'image' => 'required|string',  // Vous devrez ajuster cette règle en fonction de vos besoins
             'adresse' => 'required|string',
@@ -37,20 +37,21 @@ class EditUserRequest extends FormRequest
             'role_id' => 'required',
         ];
     }
-
+    
     public function messages()
     {
         return [
-       'nom.required' => 'Le champ nom est requis.',
-        'prenom.required' => 'Le champ prénom est requis.',
-        'email.max' => 'Le champ email ne doit pas dépasser :max caractères.',
-        'password.max' => 'Le champ mot de passe ne doit pas dépasser :max caractères.',
-        'telephone.regex' => 'Le champ numéro de téléphone doit être au format spécifié.',
-        'adresse.required' => 'Le champ adresse est requis.',
-        'etat.in' => 'Le champ état doit être actif ou inactif.',
-        'role_id.required' => 'Le champ role_id est requis.'
+            'nom.required' => 'Le champ nom est requis.',
+            'prenom.required' => 'Le champ prénom est requis.',
+            'email.max' => 'Le champ email doit avoir au maximum :max caractères.',
+            'password.min' => 'Le champ mot de passe doit avoir au moins :min 8 caractères.',
+            'telephone.regex' => 'Le champ numéro de téléphone doit être au format spécifié.',
+            'adresse.required' => 'Le champ adresse est requis.',
+            'etat.in' => 'Le champ état doit être actif ou inactif.',
+            'role_id.required' => 'Le champ role_id est requis.'
         ];
     }
+
 
     protected function failedValidation(Validator $validator)
     {
